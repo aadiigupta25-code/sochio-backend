e089ee74e6df38fafeb44bb61ef28bc12f87bb9e
aadiigupta25-code <aadiigupta25@gmail.com>
2025-11-19 02:55:54 +0530

    frtvfdwftv
    deftv

---
+++ setting.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "_id": "673eaea4529c5f0fedc97bec",
+  "privacyPolicyLink": "https://www.termsfeed.com/live/32b980c4-2fc9-4d63-b903-c9b110a553ed",
+  "privacyPolicyText": "Razor Pay",
+  "withdrawCharges": 10,
+  "withdrawLimit": 1000,
+  "createdAt": "2023-04-12T04:45:27.628Z",
+  "updatedAt": "2024-06-17T06:18:56.628Z",
+  "cancelOrderCharges": 20,
+  "paymentGateway": [
+    {
+      "name": "paytm"
+    },
+    {
+      "name": "phonePe"
+    }
+  ],
+  "adminCommissionCharges": 50,
+  "isAddProductRequest": true,
+  "razorPayId": "Razor Pay Id",
+  "razorPaySwitch": true,
+  "razorSecretKey": "rzp_test_SjZz9HC7RGCfCb",
+  "stripePublishableKey": "pk_test_51OFsHfSEMYOcpDHW9N2WeARWsZWPsnWR9reSE7shbyqUPbQpCmLv113AULXO9w6tLN1EbAmxUp3mcuCBHfJkHQJd00Qwe8Wq8v",
+  "stripeSecretKey": "sk_test_51OFsHfSEMYOcpDHWKU0EJzlAUaoU7q3f8aI8wDdOjsRP0VER1nK0Jo0Q625NaLrSztqFvc5k40yN8iEGLczHKCdD00p2Fnyzm8",
+  "stripeSwitch": true,
+  "isUpdateProductRequest": true,
+  "isFakeData": true,
+  "zegoAppId": "zego AppId",
+  "zegoAppSignIn": "zego App SignIn",
+  "privateKey": {
+    "type": "service_account",
+    "project_id": "era-shop-cf3c0",
+    "private_key_id": "a09321daa7c0b8a6ee6f40fd64a8398c2f8e8420",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCfMqU3axcn9Ppz\n/58nVSy6pLEiTQXXV0isow4hRZSKrrbxclM1QRSdNc+F8pbmd1ooD1bQFjONK4Qo\nJ6KhjWdqzKZ6/64m554Uw+KPGQ+SyrZrfRCTK5zTPk8hLoPpLWyj7WRHezZWdLPG\nVU9dbyDOYYQVxqPMV9VdmDXJFxk8m0C5dl32bPVoPUPi6kuTQ1DtA+wyA3dS/JD9\nLjeiHoSbdXjUPs2hPGgQZplg4gLtOF5YDjAZs6qIze5win4YFcagnjrZJ0XF3OQI\n6D53Qv7qIpZj0s48mH5yqy9B6AKUkLP3l8CvRVFVKKkTmnLtnWWxZzp64IfOMsVx\nRsETjoOfAgMBAAECggEACBhWr2E6LbV20NQ0PxdFLX53ZWOeQut0zhaCfu2fVof4\n1IE4OvKCvlXApjKGOtTh2DyjcPRYDbsjPrl5OxbKJfOfGY0LyX2om6y0RguDr7aw\nHPogkCM9F77I3kqTuZUuO420NjBNPIs1owWugfVHKUdrJIJTzO3Ac0Bsp7uNdHfq\nA8wvRirvjhl3IkMRUPme4+F5Bv5Ku/dHPgz/qS7wHD4rkz0IXfHy+c8Nw+aCdufY\nWxDbc0E29s9QhXSS9+yk8Fuh96M+0fcOUUqpCjlHorLsVsbAuMhyIvXYkDYfeYCR\nWav1qFKRgAhNPnUoRIDojEkevhNJjE50KOU11UrZvQKBgQDMdVuxZlFO5dCxWzj8\nhB2LxRei7TBIQyo1QC8p9cAUuioNxVtJq5L4L9/3CbAoNjxbwouofJ1ufc4ifI+X\nKxCIozRhugZ8B4VGMsfq9losile4+eeFt/3ETcm6CGWfWdoxeiKwhT34k5gwi3JJ\niFJGmtIJQ1PXgIEG+pgVhiQqTQKBgQDHVGm3LOFALv8zy2Ss96VM5TdOpheJEvho\nAQojNQXNs2l37rF1rEZ2KArVlSDZalVegUpCKRPCb7ASQqK4LnY3HUm2dJK21G/y\nT5RCYbwV3IrVS0LbeGpp/Yz0eKPxfSiFUGEGxUNRVdczOJRryyRmjbdpvaxxpnVx\nim9QrNIDmwKBgDyBFktfHfydIa76cdi4OA4emyoJytn3lkv/3HUsVlVL6hn9J1Cb\nx3hOopZ2dXzQ3/e/kJihl+0UqaWm+s8fXVsyANRSuoyFpTjq2iJ8GJ9JqUACJDN8\nCUrIQkQdLAWGD33IyO833ujqtITZH08HWmGuey1mkS8axhQMoGjuM1GZAoGAOE6X\nImByLOBZxPbStK2sK0Gg0v3GLjmGpFKO240x236v72olBRmqOdxr2xOqWOBZxz18\njZ3u8AD+2tmp/59YU49ZcEoT28O879UdC3dyWzaWnVQKSdcFMHWoaLLflOsHFEm1\nbuUwc/xssxJrktuqp7Gmux8SZHRiPrWdZku7c7sCgYEAiuA8fHiMxFMhsOLQkRqW\nraykYWkgjrasK8lCvevvmnyUJvWMjReotgGCs03RLw8Q8w7DdIRvTsKGv9Ino1Du\nvUohZiGSX6McFFoLNeaOUMPeCgG32jQZLBjIDfO5TyN9mM+LceK1WR7In5cyPO2J\n3BeTpLvM3hEyAOYCcMX1ZVs=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-dzsuv@era-shop-cf3c0.iam.gserviceaccount.com",
+    "client_id": "109638217993911454927",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-dzsuv%40era-shop-cf3c0.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  },
+  "flutterWaveId": "FLWPUBK_TEST-94c5f470ca54b0ecaca6e2b4f0985a55-X",
+  "flutterWaveSwitch": true
+};
