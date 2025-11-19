63b99904b90460ca1d7f68171f07b50f4e191285
aadiigupta25-code <aadiigupta25@gmail.com>
2025-11-19 09:50:16 +0530

    Add setting.js template

---
+++ setting.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "_id": "673eaea4529c5f0fedc97bec",
+  "privacyPolicyLink": "https://www.termsfeed.com/live/32b980c4-2fc9-4d63-b903-c9b110a553ed",
+  "privacyPolicyText": "Privacy Policy",
+  "withdrawCharges": 10,
+  "withdrawLimit": 1000,
+  "createdAt": "2023-04-12T04:45:27.628Z",
+  "updatedAt": "2024-06-17T06:18:56.628Z",
+  "cancelOrderCharges": 20,
+  "paymentGateway": [
+    {
+      "name": "paytm"
+    },
+    {
+      "name": "phonePe"
+    }
+  ],
+  "adminCommissionCharges": 50,
+  "isAddProductRequest": true,
+  "razorPayId": "your_razor_pay_id",
+  "razorPaySwitch": true,
+  "razorSecretKey": "your_razor_secret_key",
+  "stripePublishableKey": "your_stripe_publishable_key",
+  "stripeSecretKey": "your_stripe_secret_key",
+  "stripeSwitch": true,
+  "isUpdateProductRequest": true,
+  "isFakeData": true,
+  "zegoAppId": "your_zego_app_id",
+  "zegoAppSignIn": "your_zego_app_signin",
+  "privateKey": {
+    "type": "service_account",
+    "project_id": "your_project_id",
+    "private_key_id": "your_private_key_id",
+    "private_key": "your_private_key",
+    "client_email": "your_client_email",
+    "client_id": "your_client_id",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "your_client_cert_url",
+    "universe_domain": "googleapis.com"
+  },
+  "flutterWaveId": "your_flutterwave_id",
+  "flutterWaveSwitch": true
+};
